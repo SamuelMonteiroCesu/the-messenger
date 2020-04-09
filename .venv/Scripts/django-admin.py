6ee0a008cd49572a81841e869fb10a264d697de6
SamuelMonteiro <samuelsilva@alunos.unicesumar.edu.br>
2020-04-08 22:33:11 -0300

    feat: Preparo do ambiente
    Preparo do ambiente Django.
    
    * Configuração base do Django.

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\emont\Documents\Samuel\Aulas\Topicos especiais\Git_workspace\PrimeiroProjeto\the-messenger\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
